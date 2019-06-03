995c793bdd165b1fc2e7a1e1963ab90874fac609
Nitza <rondon.nitza@gmail.com>
2019-06-02 20:40:05 -0400

    code to add notes modal and update to model

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -36,56 +36,104 @@ $(".delete-from-save").on("click", function () {
 
 
 
+// NOTES
+// opens modal for entering/viewing article notes
 $(".article-notes").on("click", function () {
     // event.preventDefault();
     $("#article-note-title").empty();
-    $("#notesModal").modal("show");
+    $("#article-note-id").empty();
     // var bodyInput = $("#message-text");
     var thisId = $(this).attr("data-id");
+
     $.ajax({
-        method: "GET",
-        url: "/articles/" + thisId
-      })
-         // With that done, add the note information to the page
-    .then(function(data) {
-        console.log(data.title);
-        // The title of the article
-        $("#article-note-title").append("<h5>" + data.title + "</h5>");
-        
-        // If there's a note in the article
-        if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
-        }
-      });
-    // article-note-title
+            method: "GET",
+            url: "/articles/" + thisId
+        })
+        // With that done, add the note information to the page
+        .then(function (data) {
 
+            // The title of the article
+            $("#article-note-title").append("<h5>" + data.title + "</h5>");
+            $("#article-note-title").attr("data-id", thisId);
+            $("#article-note-id").append("<small>" + thisId + "</small>");
+
+            // If there's a note in the article
+            if (data.note) {
+                // Place the title of the note in the title input
+                $("#titleinput").val(data.note.title);
+                // Place the body of the note in the body textarea
+                $("#bodyinput").val(data.note.body);
+            }
+        });
+    $("#notesModal").modal("show");
 })
-
 
 
 
 
 // Submit Article Note button
 $(".submit-noteBtn").on("click", function () {
-    // $("#notesModal").modal("hide");
-
     var thisId = $(this).attr("data-id");
     $.ajax({
         method: "POST",
         url: "/articles/" + thisId,
         data: {
-            body: $("message-text").val(),
-            title: $("#titleinput").val()
-
+            body: $("#message-text").val(),
+            // title: $("#titleinput").val()
         }
     }).done(function (data) {
         // window.location = "/saved"
-        console.log("data");
-         // Empty the notes section
-    //   $("#notes").empty();
-        location.reload();
+        console.log(data);
+        console.log("**********data*********");
+
+        // Empty the notes section
+        //   $("#notes").empty();
+        // location.reload();
     })
+
+    $("#message-text").val("");
+});
+
+
+
+
+//Sets up a function for posting a new comment
+function postComment() {
+    //Whenever someone clicks the "Post" button...
+    $(document).on("click", ".post-comment", function () {
+        //Save the ID from that button
+        var thisId = $(this).attr("data-id");
+
+        //Send a POST request to the server
+        $.ajax({
+            method: "POST",
+            url: "/articles/" + thisId,
+            data: {
+                //Value taken from the Name input
+                name: $("#name").val(),
+                // Value taken from Comment textarea
+                body: $("#comment").val()
+            }
+        });
+
+        //Remove the values entered currently typed into the form
+        $("#name").val("");
+        $("#comment").val("");
+    });
+}
+
+
+
+
+
+
+
+
+
+
+// clear db
+$(".clearDb").on("click", function () {
+    $.get("article/clear").then(function (data) {
+        location.reload();
+    });
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,18 +23,18 @@ module.exports = function (app) {
         });
     });
 
-
     // Render Saved page
     app.get("/saved", function (req, res) {
-        db.Article.find({saved: true}).populate("notes")
-        .exec(function (error, articles) {
-            var hbsObject = {
-                article: articles
-            };
-            res.render("saved", hbsObject);
-        });
+        db.Article.find({
+                saved: true
+            }).populate("notes")
+            .exec(function (error, articles) {
+                var hbsObject = {
+                    article: articles
+                };
+                res.render("saved", hbsObject);
+            });
     });
-
 
     // A GET route for scraping the website
     app.get("/scrape", function (req, res) {
@@ -47,17 +47,11 @@ module.exports = function (app) {
             $(".story-tease-beta").each(function (i, element) {
                 // Save an empty result object
                 var result = {};
-
                 // Add the text and href of every link, and save them as properties of the result object
                 result.title = $(this).find(".story-tease-title").text();
-
                 result.link = "https://www.elnuevodia.com" + $(this).find("a").attr("href");
-
                 result.summary = $(this).find(".story-tease-summary").text();
-
                 result.articleTime = $(this).find(".story-tease-date").text();
-
-
                 // Create a new Article using the `result` object built from scraping
                 console.log(result);
 
@@ -77,8 +71,6 @@ module.exports = function (app) {
         });
     });
 
-
-
     // Route for getting all Articles from the db
     app.get("/articles", function (req, res) {
         // Grab every document in the Articles collection
@@ -94,60 +86,6 @@ module.exports = function (app) {
                 res.json(err);
             });
     });
-
-
-    // Route for grabbing a specific Article by id, populate it with it's note
-    app.get("/articles/:id", function (req, res) {
-        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-        db.Article.findOne({
-                _id: req.params.id
-            })
-            // ..and populate all of the notes associated with it
-            .populate("note")
-            .then(function (dbArticle) {
-                // If we were able to successfully find an Article with the given id, send it back to the client
-                res.json(dbArticle);
-            })
-            .catch(function (err) {
-                // If an error occurred, send it to the client
-                res.json(err);
-            });
-    });
-
-
-    // Route for saving/updating an Article's associated Note
-    app.post("/articles/:id", function (req, res) {
-        // Create a new note and pass the req.body to the entry
-        db.Note.create(req.body)
-            .then(function (dbNote) {
-                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-                return db.Article.findOneAndUpdate({
-                    _id: req.params.id
-                }, {
-                    note: dbNote._id
-                }, {
-                    new: true
-                },
-                            
-                );
-            })
-            .then(function (dbArticle) {
-                // If we were able to successfully update an Article, send it back to the client
-                res.json(dbArticle);
-            })
-            .catch(function (err) {
-                // If an error occurred, send it to the client
-                res.json(err);
-            });
-    });
-
-
-
-
-
-
 
     // Save an article
     app.put("/article/save/:id", function (req, res) {
@@ -170,9 +108,6 @@ module.exports = function (app) {
             });
     });
 
-
-    
-
     // Delete an article from saved
     app.post("/articles/delete/:id", function (req, res) {
         // Use the article id to find and update its saved boolean
@@ -193,6 +128,81 @@ module.exports = function (app) {
                 res.json(err);
             });
     });
+
+    // clear all articles from database
+    app.get("/article/clear", function (req, res) {
+        console.log(req.body)
+        db.Article.deleteMany({})
+            .then(function (dbArticle) {
+                // res.json(dbArticle);
+                res.render("/");
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+        res.send(true)
+    });
+
+
+// ARTICLES NOTES
+    // Route for grabbing a specific Article by id, populate it with it's note
+    app.get("/articles/:id", function (req, res) {
+        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+        db.Article.findOne({
+                _id: req.params.id
+            })
+            // ..and populate all of the notes associated with it
+            .populate("note")
+            .then(function (dbArticle) {
+                // If we were able to successfully find an Article with the given id, send it back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
+
+
+    // Route for saving/updating an Article's associated Note
+    app.post("/articles/:id", function (req, res) {
+        // Create a new note and pass the req.body to the entry
+        console.log(req.body)
+        console.log("***********************");
+
+
+        db.Note.create(req.body)
+            .then(function (dbNote) {
+                return db.Article.findOneAndUpdate(
+                    { _id: req.params.id },
+                    { $push: { note: dbNote._id } },
+                    { new: true }
+
+
+                //     _id: req.params.id
+                // }, {
+                //     note: dbNote._id
+                // }, {
+                //     new: true
+                // }, {
+                //     $push: {
+                //         notes: req.body.note
+                //     }
+                );
+            })
+            .then(function (dbArticle) {
+                // If we were able to successfully update an Article, send it back to the client]
+                console.log(dbArticle);
+                console.log(dbNote);
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
+
+
 
 
 
